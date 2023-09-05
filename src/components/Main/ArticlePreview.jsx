910f291cb8e39a3646cd61645a5f75bda4359d71
TypeError92 <132596140+TypeError92@users.noreply.github.com>
2023-09-05 13:11:43 +0100

    add links to individual articles to previews

--- src/components/Main/ArticlePreview.jsx
+++ src/components/Main/ArticlePreview.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
+
 function ArticlePreview({article}){
     return (
         <li className="article-preview">
-            <p>{article.title}</p>
+            <img src={article.article_img_url} alt={article.title}/>
+            <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
             <p>created @{article.created_at}</p>
             <p>by {article.author}</p>
-            <img src={article.article_img_url} alt={article.title}/>
             <p>{article.topic}</p>
         </li>
     )
