face746aa5a13e8f63dfea1a7015d0ccb4add6d0
TypeError92 <132596140+TypeError92@users.noreply.github.com>
2023-09-05 11:56:46 +0100

    add article previews

--- src/components/Main/ArticleList.jsx
+++ src/components/Main/ArticleList.jsx
@@ -1,3 +1,4 @@
+import ArticlePreview from './ArticlePreview';
 import fetchArticles from '../../api/fetchArticles'
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -21,8 +22,7 @@ function ArticleList({filter}){
         return (
             <ul className='article-list'>
                 {articles.map((article) => {
-                    console.log(article.title)
-                    return <li key={article.article_id}>{article.title}</li>
+                    return <ArticlePreview key={article.article_id} article={article}/>
                 })}
             </ul>
         )
